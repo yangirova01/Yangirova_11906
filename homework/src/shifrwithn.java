f86ea807a0368d278916130956e486a0c032c925
evelina <yangirova01@bk.ru>
2019-10-07 03:25:30 -0700

    cc

--- homework/src/shifrwithn.java
+++ homework/src/shifrwithn.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class shifrwithn {
-}
+        public static void main(String[]args){
+            Scanner scan = new Scanner(System.in);
+            int k = scan.nextInt();
+            int n = scan.nextInt();
+            int i = 0;
+
+            while (i<n){
+                int a = scan.nextInt();
+                if(a<25-k){
+                    a=a+k;
+                }
+                else {
+                    a = a%26;
+                    a=a+k;
+                }
+                i++;
+                System.out.println(a);
+            }
+        }
+    }
+
+
