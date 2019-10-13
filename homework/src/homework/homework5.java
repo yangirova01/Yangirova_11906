125ccbdb9d92595d300ebedbffef525c1e4232b2
evelina <yangirova01@bk.ru>
2019-10-13 13:58:06 -0700

    homework5

--- homework/src/homework/homework5.java
+++ homework/src/homework/homework5.java
@@ -1,7 +1,22 @@
-package homework;
-
-public class homework5 {
-    public static void main(String[] args) {
-
+package For_Study.Projects;
+import java.util.Scanner;
+public class homework2 {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt ();
+        int x = scan.nextInt ();
+        int p = 1;
+        while (n > 0) {
+            if ((n % 2) == 0) {
+                n = n/2;
+                x = x*x;
+            }
+            else {
+                n = n-1;
+                p = p*n;
+            }
+        }
+        System.out.println (x);
     }
+
 }
